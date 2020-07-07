7a2e97d611dd52357f049fed53db14cb3f7babd4
Tekin <tekinozkurt@tekins-MacBook-Air.local>
2020-07-06 21:50:43 -0400

    first commit from feature

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -4,6 +4,8 @@ public class ClassOne {
 
 	public static void main(String[] args) {
     System.out.println("ilk commit local masterdan");
+    System.out.println("ilk commit local featureden");
+
 	}
 
 }
